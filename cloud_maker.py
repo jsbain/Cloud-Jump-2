593319e23e91e120d05867cea61066f8cfc35b9c
jsbain <jsbain@yahoo.com>
2014-07-04 00:48:05 -0700

    Update cloud_maker.py
    Fixed cloud_maker.py to match original cloud style

--- cloud_maker.py
+++ cloud_maker.py
@@ -3,29 +3,33 @@ import Image, ImageDraw, random, scene
 def generate_shapes(num_circles = random.randint(4, 5)):
     shapes = []
     for i in xrange(num_circles):
-        x = i * 20 - ((num_circles/2)*30)
-        y = (random.random()-0.5) * 30
-        x += 60  # this is a hack!
-        y += 16  # this is a hack!
+        x = i * 20 - ((num_circles/2)*30)  #-60, -40, etcto +20
+        y = (random.random()-0.5) * 30   #-15 to +15
         rad = random.randint(50, 100)
         shapes.append([x, y, rad])
     return shapes
 
 def cloud_maker():
     num_circles = random.randint(4, 5)
-    #image_size = ((num_circles + 1) * 30, 60)
-    image_size = (180, 128)
+    #pil coord system 0,0 is bot left.  scene is top left.  
+    #  so, need to flip old y convention, add h
+    #  also top/bottom y get reversed in order.
+    #  finally, since scene let you draw out of the frame, we need to offset the coord sys by the min values of x and y 
+    h=100+15+5   #bottom corner rad=100, plus 15 that old cloud went up, plus 5 for shadow
+    x0=60 +2 # to push first ellipse into view
+    y0=15+5 # since old code shifted white up by 5, this was max top coord
+    image_size = (100+x0, h) 
     theImage = Image.new('RGBA', image_size) #, 'pink')
     draw = ImageDraw.Draw(theImage)
     
     circles = generate_shapes(num_circles)
     for i in circles:
-        bbox = (i[0], i[1] - 5, i[2], i[2])
+        bbox = (i[0]+x0, h-y0-i[2] , i[2]+x0, h-y0-i[1]+5)
         draw.ellipse(bbox, fill='rgb(90%,90%,90%)')
     for i in circles:
-        bbox = (i[0], i[1] + 5, i[2], i[2])
+        bbox = (i[0]+x0, h-y0-i[2] , i[2]+x0,h-y0- i[1]-5)
         draw.ellipse(bbox, fill='white')
-    
+
     del draw
     return theImage
 
